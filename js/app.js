55c5ad2051c4b67447486bc8f2bc6660f95e3089
AliRedwan <aliredwansaad@gmail.com>
2023-03-06 09:42:07 +0600

    add js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Donot show API KEY on your JS file 
+const API_KEY = `7fa8578349902098143e10fcc99d135d`
+
+const loadTemperature = city =>{
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    fetch (url)
+    .then( res => res.json())
+    .then( data => displayTemperature(data) )
+}
+
+const displayTemperature = data =>{
+    const temperature = document.getElementById('temperature');
+    console.log(data.main.temp);
+    temperature.innerText = data.main.temp;
+}
+
+loadTemperature('dhaka')
